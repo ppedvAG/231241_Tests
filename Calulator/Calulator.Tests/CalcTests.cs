e48a2ca978c0a3a29a344a0c281dca7c9b8569a4
André <AndreR@ppedv.de>
2023-10-30 10:50:12 +0100

    SumTests

---
+++ Calulator/Calulator.Tests/CalcTests.cs
@@ -0,0 +1,51 @@
+namespace Calulator.Tests
+{
+    public class CalcTests
+    {
+        [Fact]
+        public void Sum_4_and_5_results_9()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(4, 5);
+
+            //Assert
+            Assert.Equal(9, result);
+        }
+
+        [Fact]
+        public void Sum_n4_and_n5_results_n9()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(-4, -5);
+
+            //Assert
+            Assert.Equal(-9, result);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(4, 5, 9)]
+        [InlineData(-4, -5, -9)]
+        public void Sum_with_results(int a, int b, int exp)
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(a, b);
+
+            //Assert
+            Assert.Equal(exp, result);
+        }
+
+
+
+
+    }
+}
---
+++ Calulator/Calulator/Calc.cs
@@ -0,0 +1,10 @@
+﻿namespace Calulator
+{
+    public class Calc
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
