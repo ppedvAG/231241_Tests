dc73070c3704d3a02124e7b6af3164b80a8d0cc7
André <AndreR@ppedv.de>
2023-10-30 15:33:54 +0100

    Integration Tests

---
+++ TDDBank/TDDBank/CustomerManagement.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace TDDBank
+{
+    public class CustomerManagement
+    {
+        public IEnumerable<Customer> GetFromFile(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                // Return an empty collection if the file does not exist
+                return Array.Empty<Customer>();
+            }
+
+            string jsonString = File.ReadAllText(filename);
+            var customers = JsonSerializer.Deserialize<IEnumerable<Customer>>(jsonString);
+
+            return customers;
+        }
+
+        public void SaveToFile(string filename, IEnumerable<Customer> customers)
+        {
+            string jsonString = JsonSerializer.Serialize(customers, new JsonSerializerOptions {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true });
+            File.WriteAllText(filename, jsonString);
+        }
+
+    }
+
+    public class Customer
+    {
+        public required string Name { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public DateTime Birthdate { get; set; }
+    }
+}
---
+++ TDDBank/TddBank.Tests/CustomerManagementTests.cs
@@ -0,0 +1,69 @@
+﻿using Bogus;
+using System.Security.Cryptography;
+
+namespace TDDBank.Tests
+{
+    public class CustomerManagementTests
+    {
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void SaveToFile_2_test_customers()
+        {
+            var testFileName = "test.json";
+            var c1 = new Customer()
+            {
+                Name = "Fred Feuerstein",
+                Address = "Steinhausen",
+                Birthdate = DateTime.Now.AddYears(-50)
+            };
+            var c2 = new Customer()
+            {
+                Name = "Wilma Feuerstein",
+                Address = "Steinhausen",
+                Birthdate = DateTime.Now.AddYears(-40)
+            };
+            var cm = new CustomerManagement();
+
+            cm.SaveToFile(testFileName, new[] { c1, c2 });
+
+            Assert.True(File.Exists(testFileName));
+
+            File.Delete(testFileName);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void SaveToFile_2_test_customers_with_Bogus()
+        {
+            var testFileName = "test.json";
+            var cm = new CustomerManagement();
+
+            var faker = new Faker<Customer>("de");
+            faker.UseSeed(7);
+            faker.RuleFor(x => x.Name, x => x.Name.FullName());
+            faker.RuleFor(x => x.Address, x => x.Address.FullAddress());
+            faker.RuleFor(x => x.Birthdate, x => x.Date.Past(40));
+
+            cm.SaveToFile(testFileName, faker.Generate(100));
+
+            Assert.True(File.Exists(testFileName));
+
+            //File.Delete(testFileName);
+        }
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void GetFromFile_TestCustomers()
+        {
+            var testFileName = "TestCustomers.json";
+            var cm = new CustomerManagement();
+
+            var result = cm.GetFromFile(testFileName);
+
+            Assert.Equal(2, result.Count());
+        }
+
+    }
+}
+
+
