ec53153e2e4277ff4abf5894211d951e1898c3bd
André <AndreR@ppedv.de>
2023-10-30 16:25:00 +0100

    Tests mit Fakes

--- TDDBank/TDDBank/OpeningHours.cs
+++ TDDBank/TDDBank/OpeningHours.cs
@@ -1,15 +1,32 @@
-﻿namespace TDDBank
+﻿using Microsoft.Data.SqlClient;
+
+namespace TDDBank
 {
     public class OpeningHours
     {
+        public bool IsWeekend()
+        {
+            return DateTime.Now.DayOfWeek == DayOfWeek.Sunday ||
+                   DateTime.Now.DayOfWeek == DayOfWeek.Saturday;
+        }
+
+        public int GetCountEmployeesInDB()
+        {
+            var con = new SqlConnection("Server=(localdb)\\mssqllocaldb;Trusted_Connection=true;Database=PRODIVLJHFKJHBNDKJNDKJN");
+            con.Open();
+            var cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT COUNT(*) FROM Employees";
+            return (int)cmd.ExecuteScalar();
+        }
+
         public bool IsOpen(DateTime dateTime)
         {
-            if (dateTime.Day == 24 && 
+            if (dateTime.Day == 24 &&
                 dateTime.Month == 12)
                 return false;
 
             // Check if it's Monday and the time is between 10:30 and 19:00
-            if (dateTime.DayOfWeek == DayOfWeek.Monday && 
+            if (dateTime.DayOfWeek == DayOfWeek.Monday &&
                 dateTime.TimeOfDay >= new TimeSpan(10, 30, 0) &&
                 dateTime.TimeOfDay < new TimeSpan(19, 0, 0))
             {
@@ -17,7 +34,7 @@
             }
 
             // Check if it's Saturday and the time is between 13:00 and 16:00
-            if (dateTime.DayOfWeek == DayOfWeek.Saturday && 
+            if (dateTime.DayOfWeek == DayOfWeek.Saturday &&
                 dateTime.TimeOfDay >= new TimeSpan(13, 0, 0) &&
                 dateTime.TimeOfDay < new TimeSpan(16, 0, 0))
             {
@@ -27,6 +44,6 @@
             // No other opening hours specified, default to closed
             return false;
         }
-        
+
     }
 }
--- TDDBank/TddBank.Tests/OpeningHoursTests.cs
+++ TDDBank/TddBank.Tests/OpeningHoursTests.cs
@@ -1,9 +1,50 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.QualityTools.Testing.Fakes;
 
 namespace TDDBank.Tests
 {
     public class OpeningHoursTests
     {
+        [Fact]
+        public void IsWeekend_tests()
+        {
+            var oh = new OpeningHours();
+
+            using (ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 10, 30);
+                Assert.False(oh.IsWeekend());//Mo
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 10, 31);
+                Assert.False(oh.IsWeekend());//Di
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 11, 1);
+                Assert.False(oh.IsWeekend());//Mi
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 11, 2);
+                Assert.False(oh.IsWeekend());//Do
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 11, 3);
+                Assert.False(oh.IsWeekend());//Fr
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 11, 4);
+                Assert.True(oh.IsWeekend()); //Sa
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 11, 5);
+                Assert.True(oh.IsWeekend()); //So
+            }
+        }
+
+        [Fact]
+        public void GetCountEmployeesInDB_Should_return_9()
+        {
+            using (ShimsContext.Create())
+            {
+                var oh = new OpeningHours();
+                Microsoft.Data.SqlClient.Fakes.ShimSqlConnection.AllInstances.Open = (x) => { };
+                Microsoft.Data.SqlClient.Fakes.ShimSqlCommand.AllInstances.ExecuteScalar = (x) => 9;
+
+                var result = oh.GetCountEmployeesInDB();
+
+                Assert.Equal(9, result);
+            }
+        }
+
+
+
         [Theory]
         [InlineData(2023, 10, 30, 10, 30, true)]//mo
         [InlineData(2023, 10, 30, 10, 29, false)]//mo
